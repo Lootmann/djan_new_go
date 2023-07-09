35494233405e84777d351cb99c4df944cf97f4e4
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-07-10 00:06:01 +0900

    examples: example form is allow blank

--- examples/models.py
+++ examples/models.py
@@ -4,8 +4,8 @@ from words.models import WordModel
 
 
 class ExampleModel(models.Model):
-    sentence = models.TextField()
-    translation = models.TextField()
+    sentence = models.TextField(blank=True)
+    translation = models.TextField(blank=True)
     word = models.ForeignKey(WordModel, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
