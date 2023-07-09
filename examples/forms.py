8742bdd8045873d7c57ad83c24fed7985e601c9f
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-07-09 10:44:57 +0900

    examples, words: register word and examples

--- examples/forms.py
+++ examples/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 
 from examples.models import ExampleModel
@@ -7,3 +8,16 @@ class ExampleForm(ModelForm):
     class Meta:
         model = ExampleModel
         fields = ["sentence", "translation"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        sentence = cleaned_data["sentence"]
+        translation = cleaned_data["translation"]
+
+        # when both sentence and translation are empty,
+        # do not save this to ExampleModel.
+        if sentence is None or sentence == "":
+            if translation is None or translation == "":
+                raise forms.ValidationError("Empty")
+
+        return cleaned_data
