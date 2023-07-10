72b702039e8642d2819c64ad39a2fa5b340313db
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-07-10 12:39:13 +0900

    quiz: show quiz

---
+++ static/js/quiz/index.js
@@ -0,0 +1,18 @@
+// show meaning when click quiz-header
+const meaning = document.querySelector("#meaning");
+
+const quiz_header = document.getElementById("quiz-header");
+quiz_header.addEventListener("click", function () {
+  meaning.classList.toggle("hidden");
+});
+
+// Press arrow right
+document.addEventListener("keydown", (e) => {
+  const key = e.key;
+
+  switch (key) {
+    case "ArrowRight":
+      window.location.href = "";
+      break;
+  }
+});
